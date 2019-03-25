33ee221a7b4f34833808d0794de8468868e3a021
Bob Hunt <bob.hunt.email@gmail.com>
2019-03-25 10:38:23 -0400

    Added a favorite attribute to Recipe model in models.py.

--- recipe_box/recipe_box/models.py
+++ recipe_box/recipe_box/models.py
@@ -15,6 +15,9 @@ class Recipe(models.Model):
     description = models.CharField(max_length=50)
     time = models.CharField(max_length=50)
     instructions = models.TextField()
+    # *** Need to uncomment the following line, then makemigrations and migrate. ***
+    # favorite = models.ManyToManyField("self", related_name='favorite_of', symmetrical=False)
+
 
     def __str__(self):
         return self.title
